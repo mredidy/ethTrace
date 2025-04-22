1a5538c2283c463f849f828ceab7578607d6c95f
Edidiong Akpan <100961974+mredidy@users.noreply.github.com>
2025-04-22 12:01:21 +0000

    Update

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
-import os
-import requests
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
-ETHERSCAN_API = "https://api.etherscan.io/api"
-ETHERSCAN_KEY = os.getenv("ETHERSCAN_API_KEY")
+@app.get("/")
+def root():
+    return {"message": "Hello from FastAPI + Replit + Render!"}
 
-@app.get("/latest-tx")
-def latest_tx():
-    params = {
-        "module": "account",
-        "action": "txlist",
-        "address": "0xdAC17F958D2ee523a2206206994597C13D831ec7",  # USDT Token
-        "startblock": 0,
-        "endblock": 99999999,
-        "page": 1,
-        "offset": 5,
-        "sort": "desc",
-        "apikey": ETHERSCAN_KEY,
-    }
-    response = requests.get(ETHERSCAN_API, params=params)
-    return response.json()
+@app.get("/ping")
+def ping():
+    return {"ping": "pong"}
